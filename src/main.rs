aad005965503aca8d77a4e8f8911f46eb94dc31d
vladnishe <vladmasliev777@gmail.com>
2026-09-23 19:51:12 +0300

    finish first rust project !

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::prelude::*;
+
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    let secret_number = rand::rng().random_range(1..=100);
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Faield to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faield to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
